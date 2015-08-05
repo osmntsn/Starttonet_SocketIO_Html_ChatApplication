fc507ef06a3c2935923fea2d2e35a8c96b2aec4b
osmntsn <osmantosun38@gmail.com>
2015-08-05 11:32:37 +0300

    deneme

---
+++ Scripts/ChatApplication.js
@@ -0,0 +1,109 @@
+﻿var apiKey = '44b0e2ad';
+var name = "not_set";
+function connect(socketName) {
+    console.log("Trying to connect starttonet server with apiKey:" + apiKey);
+    connecttoStarttonet(apiKey, socketName);
+}
+function onConnect() {
+    $("#disconnectedAlert").fadeOut("fast", function () {
+        $("#connectedAlert").fadeIn("fast", function () {
+        });
+    });
+    socket.emit("getConnectedUsers");
+}
+function onDisconnect() {
+    $("#connectedAlert").fadeOut("fast", function () {
+        $("#disconnectedAlert").fadeIn("fast", function () {
+        });
+    });
+    document.getElementById('userArea').innerHTML = "";
+}
+function onError(errorMessage) {
+    console.log(errorMessage);
+}
+
+function onData(data) {
+    console.log("Data Recived:" + data);
+    var recivedData = JSON.parse(data);
+    if (recivedData['dc_MessageTo'] == "All") {
+        $('#messageArea').append(recivedData['dc_UserName'] + ":" + recivedData["dc_Message"] + "</br>");
+        $("#messageArea").animate({ scrollTop: $('#messageArea')[0].scrollHeight }, 1000);
+    }
+    else {
+        if (recivedData['dc_MessageTo'] == name) {
+            var message = recivedData["dc_UserName"] + ": " + recivedData["dc_Message"];
+            document.getElementById('privateMessageDiv').innerHTML = message;
+            $('#messageModal').modal('show');
+        }
+
+    }
+}
+function onGetColumns(columns) {
+
+}
+function onSocketDisconnected(socketName) {
+
+    var userList = document.getElementById('userArea').innerHTML;
+    console.log(socketName + ' disconnected ' + userList);
+    document.getElementById('userArea').innerHTML = userList.replace("<div>" + socketName + "</div>", "");
+    $('#messageArea').append(socketName + " Sohbetten Ayrıldı</br>");
+}
+function onSocketConnected(socketName) {
+    if (document.getElementById('userArea').innerHTML.indexOf(socketName) < 0) {
+        $('#userArea').append("<div>" + socketName + "</div>");
+        $('#messageArea').append(socketName + " Sohbete Bağlandı</br>");
+        console.log(socketName + ' connected');
+    }
+}
+function getUserList(users) {
+    console.log('users:' + users);
+    $.each(users, function (key, value) {
+        //$('#kullanicilar').append("<div>" + key + "</div>");
+        if (document.getElementById('userArea').innerHTML.indexOf(value) < 0)
+            $('#userArea').append("<div>" + value + "</div>");
+    });
+}
+
+/*function onGetDataFromSocket(data) {
+    data = JSON.parse(data);
+    var message = data["dc_UserName"] + ": " + data["dc_Message"];
+    document.getElementById('privateMessageDiv').innerHTML = message;
+    $('#messageModal').modal('show');
+}*/
+
+$(function () {
+    $('#sendButton').click(function () {
+        var message = $('#messageText').val();
+        message = message;
+        var columns = ["dc_UserName", "dc_Message", "dc_MessageTo"];
+        var data = [name, message,"All"];
+        createAndSendData(columns, data, "All");
+    });
+});
+
+$(function () {
+    $('#nameSetButton').click(function () {
+        name = $('#nameText').val();
+        if (name.length >= 3) {
+            connect(name);
+            $("#namePanel").fadeOut("slow", function () {
+                $("#MessagingPanel").fadeIn("slow", function () {
+                });
+            });
+        }
+        else
+            alert("İsim alanı 3 karakterden fazla olmalı");
+    });
+});
+
+
+$(function () {
+    $('#modalSend').click(function () {
+        var message = $('#modalText').val();
+        var user = $('#modalUser').val();
+        var columns = ["dc_UserName", "dc_Message","dc_MessageTo"];
+        var data = [name, message,user];
+        console.log("private message send to " + user);
+        createAndSendData(columns, data, user);
+    });
+});
---
+++ Scripts/starttonetScripts.js
@@ -0,0 +1,114 @@
+﻿var socket = null;
+var columns = [];
+function connecttoStarttonet(apiKey,socketName) {
+    if (socket == null) {
+        socket = io.connect("http://localhost:8888");
+
+        socket.on("connect", function () {
+            socket.emit("firstConnection", apiKey,socketName);
+            console.log("connected");
+            onConnect();
+        });
+
+        socket.on("getDataFromServer", function (data) {
+            console.log("data recived");
+            onData(data);
+        });
+
+        socket.on("getDataColumns", function (columns) {
+            onGetColumns(columns);
+            this.columns = columns;
+        });
+
+        socket.on('disconnect', function () {
+            onDisconnect();
+            console.log('disconnected');
+        });
+
+        socket.on('sendError', function (errorMessage) {
+            onError(errorMessage);
+            console.log('Error:' + errorMessage);
+        });
+
+        socket.on('socketDisconnected', function (socketName) {
+            onSocketDisconnected(socketName);
+        });
+
+        socket.on('socketConnected', function (socketName) {
+            onSocketConnected(socketName);
+        });
+        socket.on('getUserList', function (users) {
+            getUserList(users);
+        });
+
+        socket.on('getDataFromSocket', function (data) {
+            onGetDataFromSocket(data);
+            console.log(data);
+        });
+        
+    }
+    else {
+        console.log("Already connected");
+    }
+}
+function sendDataToServer(data, socketName) {
+
+    console.log("data sended:" + data + " to " + socketName);
+    socket.emit("sendDataFromClient", data, apiKey, socketName);
+    
+}
+function sendtDataToSocket(columnNameArray, dataArray,socketName) {
+
+    if (typeof columnNameArray.length == "undefined" || typeof dataArray.length == "undefined") {
+        console.log("Undefined array");
+        return 0;
+    }
+
+    if (columnNameArray.length != dataArray.length) {
+        console.log("Length of arrays must be same!");
+        return 0;
+    }
+   
+   /* for (var i = 0 ; i < columnNameArray.length; i++) {
+        result = result + '"' + columnNameArray[i] + '":' + '"' + dataArray[i] + '",';
+    }
+    result = result.substring(0, result.length - 1);
+    result = result + '}';*/
+
+    var obj = {};
+    for (var i = 0; i < columnNameArray.length; i++) {
+        obj[columnNameArray[i]] = dataArray[i];
+    }
+    var result = JSON.stringify(obj);
+    socket.emit('sendDataToSocket', result, socketName);
+    console.log("data sended to socket:"+socketName+" data:"+result);
+}
+function getColumns() {
+    socket.emit("getDataColumns", apiKey);
+    console.log("get data columns for:"+apiKey);
+}
+function createAndSendData(columnNameArray, dataArray,socketName) {
+
+    if (typeof columnNameArray.length == "undefined" || typeof dataArray.length == "undefined") {
+        console.log("Undefined array");
+        return 0;
+    }
+
+    if (columnNameArray.length != dataArray.length) {
+        console.log("Length of arrayys must be same");
+        return 0;
+    }
+    /*var result = "{";
+    for (var i = 0 ; i < columnNameArray.length; i++) {
+        result = result + '"' + columnNameArray[i] + '":' + '"' + dataArray[i] + '",';
+    }
+    result = result.substring(0, result.length - 1);
+    result = result + '}';*/
+
+    var obj = {};
+    for (var i = 0; i < columnNameArray.length; i++) {
+        obj[columnNameArray[i]] = dataArray[i];
+    }
+    result = JSON.stringify(obj);
+    sendDataToServer(result,socketName);
+}
